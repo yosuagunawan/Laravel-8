4ab8973b4e61b5b774479a6db7f039c742cff6f5
Yosua G <yosua.gunawan002@binus.ac.id>
2022-12-08 09:55:23 +0700

    Error Handling Registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -12,8 +12,15 @@ class RegisterController extends Controller
             'title' => 'Register'
         ]);
     }
-    public function store()
+    public function store(Request $request)
     {
-        return request()->all();
+        $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        dd('registrasi berhasil');
     }
 }
